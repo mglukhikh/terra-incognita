05437334dd45a1d728baacdf03c2d87940224848
Mikhail Glukhikh <Mikhail.Glukhikh@jetbrains.com>
2017-03-10 14:32:58 +0300

    Some comments

--- src/main/kotlin/ru/spbstu/terrai/core/MoveResult.kt
+++ src/main/kotlin/ru/spbstu/terrai/core/MoveResult.kt
@@ -1,8 +1,12 @@
 package ru.spbstu.terrai.core
 
 class MoveResult(
+        // Which room was at target location
         val room: Room,
+        // Condition after move (contains list of items and exit reaching flag)
         val condition: Condition,
+        // true if move was successful
         val successful: Boolean,
+        // status string (normally should not be analyzed)
         val status: String
 )
