3d11880b7f203221d032a3721ab374eccad01c87
Mikhail Glukhikh <Mikhail.Glukhikh@jetbrains.com>
2017-03-10 11:27:41 +0300

    Second labyrinth sample (unsolvable for brain dead player)

--- src/test/kotlin/ru/spbstu/terrai/players/samples/BrainDeadTest.kt
+++ src/test/kotlin/ru/spbstu/terrai/players/samples/BrainDeadTest.kt
@@ -15,4 +15,11 @@ class BrainDeadTest {
         assertEquals(Controller.GameResult(4, exitReached = true), controller.makeMoves(100))
     }
 
+    @Test
+    fun testLab2() {
+        val lab = Labyrinth.createFromFile("labyrinths/lab2.txt")
+        val player = BrainDead()
+        val controller = Controller(lab, player)
+        assertEquals(Controller.GameResult(100, exitReached = false), controller.makeMoves(100))
+    }
 }
